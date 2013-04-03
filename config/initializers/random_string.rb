c82aab6ad2d3df79bb1b68a1ae192e0bce1bc7cc
ruthenium <ruthenium@localhost>
2013-04-04 03:51:46 +0400

    length

--- config/initializers/random_string.rb
+++ config/initializers/random_string.rb
@@ -2,7 +2,7 @@ class String # Monkey patching is bad, yeah.
   class << self
     AsciiAlphanum = [0..9, 'a'..'z', 'A'..'Z'].map(&:to_a).flatten
 
-    def random(length=0)
+    def random(length)
       # Note! Ruby version > 1.9 required for Array#sample
       AsciiAlphanum.sample(length).join
       # SecureRandom.hex(3)
