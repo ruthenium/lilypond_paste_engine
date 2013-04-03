e0ade52ade22e210f8eb8b6eecb9d7523c547a7b
ruthenium <ruthenium@localhost>
2013-04-03 21:58:07 +0400

    migrate fix

--- db/migrate/20130323162912_add_visible_id_index_to_paste.rb
+++ db/migrate/20130323162912_add_visible_id_index_to_paste.rb
@@ -1,6 +1,5 @@
 class AddVisibleIdIndexToPaste < ActiveRecord::Migration
   def change
-    #add_column :pastes, :visible_id, :string, :limit => 6, :null => false, :default => 'ffffff'
     add_index :pastes, :visible_id, :unique => true
   end
 end
